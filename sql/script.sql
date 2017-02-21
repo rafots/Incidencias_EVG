60af3b73316a6be111b2f7779e72157d558b5811
Rafa <rafapacense@gmail.com>
2017-02-21 13:49:44 +0100

    ADD SCRIPT CREACIÓN BASE DE DATOS Y DIAGRAMA

---
+++ sql/script.sql
@@ -0,0 +1,141 @@
+/* 20/02/2017 */
+
+
+CREATE DATABASE IF NOT EXISTS IncidenciasEVG;
+USE IncidenciasEVG;
+
+/* TABLA 1 - PROFESORES*/
+CREATE TABLE IF NOT EXISTS profesores(
+  idUsuario TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  usuario VARCHAR(20) NOT NULL UNIQUE,
+  correo VARCHAR(60) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  pass VARCHAR(255) NOT NULL,
+  tipo CHAR(1) NOT NULL,
+  baja_temporal BIT NOT NULL
+);
+
+/* TABLA 2 - ETAPAS*/
+CREATE TABLE IF NOT EXISTS etapas(
+  codEtapa CHAR(5) PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL,
+  coordinador TINYINT NOT NULL,
+  CONSTRAINT fk_etapas_profesores FOREIGN KEY (coordinador) REFERENCES profesores(idUsuario)
+);
+
+/* TABLA 3 - SECCIONES*/
+CREATE TABLE IF NOT EXISTS secciones(
+  idSeccion CHAR(6) NOT NULL PRIMARY KEY,
+  nombre VARCHAR(60) NOT NULL,
+  tutor TINYINT UNSIGNED NOT NULL,
+  CONSTRAINT fk_tutores_secciones FOREIGN KEY (tutor) REFERENCES profesores(idUsuario)
+);
+
+/* TABLA 4-ALUMNOS */
+CREATE TABLE  IF NOT EXISTS alumnos(
+  nia CHAR (7) PRIMARY KEY,
+  nombreCompleto VARCHAR(50) NOT NULL,
+  idSeccion CHAR(6) NOT NULL ,
+  CONSTRAINT fk_secciones_alumnos  FOREIGN KEY (idSeccion) REFERENCES secciones(idSeccion)
+);
+
+/*TABLA 5 - PROFESORES-SECCION*/
+CREATE TABLE IF NOT EXISTS profesores_seccion(
+  profesor TINYINT UNSIGNED,
+  idSeccion CHAR(5),
+  PRIMARY KEY (profesor,idSeccion),
+  CONSTRAINT fk_profesor_prof_secc FOREIGN KEY (profesor) REFERENCES profesores(idUsuario),
+  CONSTRAINT fk_seccion_prof_secc FOREIGN KEY (idSeccion) REFERENCES secciones(idSeccion)
+);
+
+/*Tabla 6- tipos_incidencias*/
+CREATE TABLE tipo_Incidencias(
+  idTipo tinyint UNSIGNED PRIMARY KEY,
+  nombre varchar(30) NOT NULL,
+  codEtapa CHAR(5) NOT NULL,
+  CONSTRAINT fk_tipos_incidencias FOREIGN KEY (codEtapa) REFERENCES etapas(codEtapa)
+);
+
+/*TABLA 9 - HORAS*/
+CREATE TABLE IF NOT EXISTS horas(
+  idHora TINYINT UNSIGNED PRIMARY KEY,
+  nombre VARCHAR(20) NOT NULL
+);
+
+/*TABLA 10 - INCIDENCIAS*/
+CREATE TABLE IF NOT EXISTS incidencias(
+  idIncidencia SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  nia CHAR(7) NOT NULL,
+  idTipo TINYINT NOT NULL,
+  usuario TINYINT NOT NULL,
+  codAsignatura VARCHAR(30) NULL,
+  idHora TINYINT NOT NULL,
+  fecha_ocurrencia DATE NOT NULL,
+  fecha_registro DATETIME NOT NULL,
+  descripcion VARCHAR(300) NOT NULL,
+  leidaT BIT NOT NULL,
+  leidaC BIT NOT NULL,
+  gestionada BIT NOT NULL,
+  CONSTRAINT fk_incidencias_alumno FOREIGN KEY (nia) REFERENCES alumnos(nia),
+  CONSTRAINT fk_incidencias_tipo FOREIGN KEY (idTipo) REFERENCES tipo_Incidencias(idTipo),
+  CONSTRAINT fk_incidencias_profesor FOREIGN KEY (usuario) REFERENCES profesores(idUsuario),
+  CONSTRAINT fk_incidencias_asignatura FOREIGN KEY (codAsignatura) REFERENCES asignaturas(codAsignatura),
+  CONSTRAINT fk_incidencias_hora FOREIGN KEY (idHora) REFERENCES horas(idHora)
+);
+
+/*Tabla 11-tipos_Anotaciones*/
+CREATE TABLE tipos_Anotaciones(
+  tipoAnotacion TINYINT PRIMARY KEY,
+  nombre varchar(40) NOT NULL,
+  codEtapa CHAR(5) NOT NULL,
+  CONSTRAINT fk_tipos_Anotaciones FOREIGN KEY (codEtapa) REFERENCES etapas(codEtapa)
+);
+
+/* TABLA 13-ANOTACIONES */
+CREATE TABLE IF NOT EXISTS anotaciones(
+  numAnotacion SMALLINT UNSIGNED PRIMARY KEY,
+  tipoAnotacion TINYINT NOT NULL,
+  nia CHAR(7) NOT NULL ,
+  hora_Registro DATETIME NOT NULL,
+  userCreacion CHAR(1) NOT NULL ,
+  leida BIT(1) NOT NULL,
+  CONSTRAINT anotaciones_1 FOREIGN KEY (tipoAnotacion)
+  REFERENCES tipos_anotaciones_etapas (tipoAnotacion),
+  CONSTRAINT anotaciones_2 FOREIGN KEY (nia) REFERENCES alumnos(nia)
+);
+
+/* TABLA 14 - TIPO_SANCION*/
+CREATE TABLE IF NOT EXISTS tipo_sancion(
+  tipoSancion TINYINT UNSIGNED NOT NULL PRIMARY KEY,
+  nombre VARCHAR(20) NOT NULL
+);
+
+/* TABLA 15 - SANCIONES*/
+CREATE TABLE IF NOT EXISTS sanciones(
+  idSancion SMALLINT UNSIGNED NOT NULL PRIMARY KEY,
+  idIncidencia SMALLINT UNSIGNED NOT NULL,
+  tipoSancion TINYINT  UNSIGNED NOT NULL,
+  fecha_inicio DATE NOT NULL,
+  fecha_fin DATE,
+  observacion VARCHAR(300) NOT NULL,
+  CONSTRAINT fk_incidencias_sanciones FOREIGN KEY (idIncidencia) REFERENCES incidencias(idIncidencia),
+  CONSTRAINT fk_tipo_sancion_sanciones FOREIGN KEY (tipoSancion) REFERENCES tipo_sancion(tipoSancion)
+);
+
+/* TABLA 16 - TIPO_SANCION_INCIDENCIAS*/
+CREATE TABLE IF NOT EXISTS tipo_sancion_incidencias(
+  tipoSancion TINYINT UNSIGNED NOT NULL,
+  idTipo TINYINT  UNSIGNED NOT NULL,
+  PRIMARY KEY (tipoSancion,idTipo),
+  CONSTRAINT fk_tipo_sancion_incidencias_1 FOREIGN KEY (tipoSancion) REFERENCES tipo_sancion(tipoSancion),
+  CONSTRAINT fk_tipo_sancion_incidencias_2 FOREIGN KEY (idTipo) REFERENCES tipo_Incidencias(idTipo)
+);
+
+/* TABLA 17-GESTION */
+CREATE TABLE IF NOT EXISTS gestion(
+  idUsuario TINYINT UNSIGNED PRIMARY KEY,
+  nombre VARCHAR(20) NOT NULL,
+  pass VARCHAR(255) NOT NULL,
+  tipo CHAR(1) NOT NULL
+
+);
