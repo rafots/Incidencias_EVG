8684390493c9c6ed6cd2b55d43cedd2364983fd2
Rafa <rafapacense@gmail.com>
2017-02-23 10:59:20 +0100

    ADD CLASE CONEXION Y PROCEDIMIENTOS

---
+++ procedimientos/procedimientos.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: 2daw10
+ * Date: 23/02/2017
+ * Time: 8:58
+ */
+require '../conexion/conexion.php';
+class procedimientos
+{
+    private $conexion;
+    private $resultado;
+    public function conectar()
+    {
+        $obj = new conexion();
+        $this->conexion = new mysqli($obj->getServer(),$obj->getUser(),$obj->getPass(),$obj->getDb());
+        $this->conexion->set_charset("UTF8");
+        if($this->conexion->connect_error)
+        {
+            $this->conexion->connect_error;
+        }
+    }
+    public function consultas($query)
+    {
+        $this->resultado = $this->conexion->query($query);
+    }
+    public function consultasPreparadas($query)
+    {
+        $this->conexion->prepare($query);
+    }
+    public function devolverFilas()
+    {
+        $this->resultado->fetch_array();
+    }
+    public function filasAfectadas()
+    {
+        $this->conexion->affected_rows;
+    }
+    public function errores()
+    {
+        $this->conexion->error;
+    }
+    public function cerrarConexion()
+    {
+        $this->conexion->close;
+    }
+
+
+
+}
