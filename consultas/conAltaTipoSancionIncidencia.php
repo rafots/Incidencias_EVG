0c42dc6792cb95dbf01af124ba6ea8f497caa47f
KratosMercer <juan.1996.12@hotmail.com>
2017-03-01 23:04:14 +0100

    ADD: Alta de tipos de sanciones por incidencias ya funcionable

---
+++ consultas/conAltaTipoSancionIncidencia.php
@@ -0,0 +1,13 @@
+<?php
+    require '../conexion/conexion.php';
+    $conexion = new conexion();
+    $conectar = new mysqli($conexion->getServer(),$conexion->getUser(),$conexion->getPass(),$conexion->getDb());
+    $alta_tipo_sanc_inc="INSERT INTO tipo_sancion_incidencias VALUES (?,?)";
+    $stmt=$conectar->prepare($alta_tipo_sanc_inc);
+
+    $stmt->bind_param('ii',$_POST["tipoSancion"],$_POST["tipoIncidencia"]);
+
+    $stmt->execute();
+    header("Location:../paginas/addTipoSancionIncidencia.php?consulta=ok");
+
+?>
