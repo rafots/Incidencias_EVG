aa7f20ce6692c332965b40cca3cfd5289df5b3f1
LueW <jorgedaw44@gmail.com>
2017-03-03 13:10:06 +0100

    ADD: addSancion, comun.js UPDATE: sanciones.html script.sql (TABLE sanciones, alumnos) procedimientos.php

--- procedimientos/procedimientos.php
+++ procedimientos/procedimientos.php
@@ -11,39 +11,49 @@ class procedimientos
 {
     private $conexion;
     private $resultado;
+
     public function conectar()
     {
         $obj = new conexion();
-        $this->conexion = new mysqli($obj->getServer(),$obj->getUser(),$obj->getPass(),$obj->getDb());
+        $this->conexion = new mysqli($obj->getServer(), $obj->getUser(), $obj->getPass(), $obj->getDb());
         $this->conexion->set_charset("UTF8");
-        if($this->conexion->connect_error)
-        {
+        if ($this->conexion->connect_error) {
             $this->conexion->connect_error;
         }
     }
+
     public function consultas($query)
     {
         $this->resultado = $this->conexion->query($query);
     }
+
     public function consultasPreparadas($query)
     {
         return $this->conexion->prepare($query);
     }
+
     public function devolverFilas()
     {
         $this->resultado->fetch_array();
     }
+
     public function filasAfectadas()
     {
         $this->conexion->affected_rows;
     }
+
     public function errores()
     {
         $this->conexion->error;
     }
+
     public function cerrarConexion()
     {
         return $this->conexion->close();
     }
 
+    public function ultimoId()
+    {
+        return $this->conexion->insert_id;
+    }
 }
