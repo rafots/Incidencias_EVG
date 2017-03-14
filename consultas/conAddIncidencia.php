6f0f99ebac584043199beab68de8971877569087
juan <juan.1996.12@hotmail.com>
2017-03-14 10:24:16 +0100

    UPDATE: cambios en alta incidencia

--- consultas/conAddIncidencia.php
+++ consultas/conAddIncidencia.php
@@ -3,15 +3,15 @@
     /*INSERT INTO `incidencias` VALUES (NULL, '092323A', '2', '1', 'Recreo', '1', '2017-03-01', '2017-03-08 08:32:19', 'Yasuo', '0', '0', '0', '0');*/
     include "../procedimientos/procedimientos.php";
     $conexion = new procedimientos();
-    $conexion = conectar();
+    $conexion->conectar();
 
     $consulta_user="SELECT idUsuario FROM profesores WHERE usuario='".$_SESSION["usuario"]."'";
-    $resultado_user=$conectar->query($consulta_user);
-    $fila_usuario=$resultado_user->fetch_array();
+    $conexion->consultas($consulta_user);
+    $fila_usuario=$conexion->devolverFilas();
 
     $consulta_tipo="SELECT * FROM tipo_incidencias WHERE idTipo=".$_POST["tipo_inc"]."";
-    $resultado_tipo=$conectar->query($consulta_tipo);
-    $fila_tipo=$resultado_tipo->fetch_array();
+    $conexion->consultas($consulta_tipo);
+    $fila_tipo=$conexion->devolverFilas();
 
     if($fila_tipo["gestiona"]=="T"){
         $VT=0; /*Vista por el tutor*/
@@ -25,8 +25,10 @@
         $VC=0; /*Vista por el coordinador*/
         $AC=0; /*Archivada por el coordinador*/
     }
-
+    if($_POST["asignatura"]==NULL){
+        $_POST["asignatura"]='Vacio';
+    }
     $consulta_add_incidencia="INSERT INTO incidencias VALUES (DEFAULT, '".$_POST["nia"]."', ".$_POST["tipo_inc"].", ".$fila_usuario["idUsuario"].", '".$_POST["asignatura"]."', ".$_POST["hora"].", '".$_POST["fecha_incidencia"]."', NOW(), '".$_POST["descripcion"]."', '".$VT."', '".$AT."', '".$VC."', '".$AC."');";
-    $resultado_add_incidencia=$conectar->query($consulta_add_incidencia);
+    $conexion->consultas($consulta_add_incidencia);
     header("Location: ../paginas/profesor.php?alta_inc=si");
 ?>
