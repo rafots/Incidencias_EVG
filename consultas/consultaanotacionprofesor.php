a3599e635c0ea15d0be490c012823b3198a7df75
Juank <jromanamador@gmail.com>
2017-03-17 00:32:08 +0100

    UPDATE un campo numerico que no encajaba

--- consultas/consultaanotacionprofesor.php
+++ consultas/consultaanotacionprofesor.php
@@ -3,7 +3,7 @@ require_once '../procedimientos/procedimientos.php';
 
         $objeto = new procedimientos();
         $objeto->conectar();
-        $consulta="Select tipos_anotaciones.tipoAnotacion as tipoAnotaciones,tipos_anotaciones.nombre as nombre,anotaciones.nia as 'Nia del alumno',nombreCompleto, numAnotacion,substring(hora_Registro,1,11)as fecha,descripcion,alumnos.idSeccion,profesores_seccion.idSeccion from (SELECT * FROM anotaciones LIMIT 10)
+        $consulta="Select tipos_anotaciones.nombre as tipoAnotaciones,tipos_anotaciones.nombre as nombre,anotaciones.nia as 'Nia del alumno',nombreCompleto, numAnotacion,substring(hora_Registro,1,11)as fecha,descripcion,alumnos.idSeccion,profesores_seccion.idSeccion from (SELECT * FROM anotaciones LIMIT 10)
     anotaciones inner join tipos_anotaciones on tipos_anotaciones.tipoAnotacion=anotaciones.tipoAnotacion
     inner join alumnos on anotaciones.nia=alumnos.nia
     inner join secciones on secciones.idSeccion=alumnos.idSeccion
